3a343574fca6f6858ee01ef0d889d588d57b5546
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2022-11-10 21:45:56 +0300

    Exceptions fixed
    Exceptions fixed

--- scripts.py
+++ scripts.py
@@ -4,6 +4,41 @@ import sys
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datacenter.models import Chastisement, Commendation, Lesson, Mark, Schoolkid, Subject
 
+appraisals = [
+        "Молодец!",
+        "Отлично!",
+        "Хорошо!",
+        "Гораздо лучше, чем я ожидал!",
+        "Ты меня приятно удивил!",
+        "Великолепно!",
+        "Прекрасно!",
+        "Ты меня очень обрадовал!",
+        "Именно этого я давно ждал от тебя!",
+        "Сказано здорово – просто и ясно!",
+        "Ты, как всегда, точен!",
+        "Очень хороший ответ!",
+        "Талантливо!",
+        "Ты сегодня прыгнул выше головы!",
+        "Я поражен!",
+        "Уже существенно лучше!",
+        "Потрясающе!",
+        "Замечательно!",
+        "Прекрасное начало!",
+        "Так держать!",
+        "Ты на верном пути!",
+        "Здорово!",
+        "Это как раз то, что нужно!",
+        "Я тобой горжусь!",
+        "С каждым разом у тебя получается всё лучше!",
+        "Мы с тобой не зря поработали!",
+        "Я вижу, как ты стараешься!",
+        "Ты растешь над собой!",
+        "Ты многое сделал, я это вижу!",
+        "Теперь у тебя точно все получится!"
+        ]
+    
+random_appraisal = random.choice(appraisals)
+
 
 def fix_marks(child):
 	Mark.objects.filter(schoolkid_id=child.id, points__lt=4).update(points=5)
@@ -38,52 +73,17 @@ def main():
 		print("Empty name provided")
 	try:
 		child = Schoolkid.objects.get(full_name__contains=name)
-	except MultipleObjectsReturned:
+	except Schoolkid.MultipleObjectsReturned:
 		print("There are multiple IDs with provided name.")
-	except ObjectDoesNotExist:
+	except Schoolkid.ObjectDoesNotExist:
 		print("Schoolkid entry doesn't exist in database.")
 
 	if not school_subject:
 		print("Empty school_subject provided")
 	try:
         	school_subject = Subject.objects.get(title=school_subject, year_of_study=child.year_of_study)
-    	except ObjectDoesNotExist:
-        	print(f"{school_subject} not found")
-    
-    appraisals = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!",
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Очень хороший ответ!",
-        "Талантливо!",
-        "Ты сегодня прыгнул выше головы!",
-        "Я поражен!",
-        "Уже существенно лучше!",
-        "Потрясающе!",
-        "Замечательно!",
-        "Прекрасное начало!",
-        "Так держать!",
-        "Ты на верном пути!",
-        "Здорово!",
-        "Это как раз то, что нужно!",
-        "Я тобой горжусь!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!",
-        "Ты растешь над собой!",
-        "Ты многое сделал, я это вижу!",
-        "Теперь у тебя точно все получится!"
-        ]
-    
-    random_appraisal = random.choice(appraisals)
+    	except Subject.ObjectDoesNotExist:
+        	print(f"{school_subject} not found")  
 
     fix_marks(child)
     remove_chastisements(child)
