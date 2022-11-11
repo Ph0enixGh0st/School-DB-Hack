1384f0dad2dce27f3511e75c349e8590be3aa0ec
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2022-11-11 22:58:45 +0300

    Variables and functions update
    - APPRAISALS turned into a global variable now
    - random appraisal moved inside create_commendation
    - System exits added when no name or school subject provided by the user
    - PEP8 reformatted

--- scripts.py
+++ scripts.py
@@ -4,40 +4,38 @@ import sys
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datacenter.models import Chastisement, Commendation, Lesson, Mark, Schoolkid, Subject
 
-appraisals = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!",
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Очень хороший ответ!",
-        "Талантливо!",
-        "Ты сегодня прыгнул выше головы!",
-        "Я поражен!",
-        "Уже существенно лучше!",
-        "Потрясающе!",
-        "Замечательно!",
-        "Прекрасное начало!",
-        "Так держать!",
-        "Ты на верном пути!",
-        "Здорово!",
-        "Это как раз то, что нужно!",
-        "Я тобой горжусь!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!",
-        "Ты растешь над собой!",
-        "Ты многое сделал, я это вижу!",
-        "Теперь у тебя точно все получится!"
-        ]
-    
-random_appraisal = random.choice(appraisals)
+APPRAISALS = [
+	"Молодец!",
+	"Отлично!",
+	"Хорошо!",
+	"Гораздо лучше, чем я ожидал!",
+	"Ты меня приятно удивил!",
+	"Великолепно!",
+	"Прекрасно!",
+	"Ты меня очень обрадовал!",
+	"Именно этого я давно ждал от тебя!",
+	"Сказано здорово – просто и ясно!",
+	"Ты, как всегда, точен!",
+	"Очень хороший ответ!",
+	"Талантливо!",
+	"Ты сегодня прыгнул выше головы!",
+	"Я поражен!",
+	"Уже существенно лучше!",
+	"Потрясающе!",
+	"Замечательно!",
+	"Прекрасное начало!",
+	"Так держать!",
+	"Ты на верном пути!",
+	"Здорово!",
+	"Это как раз то, что нужно!",
+	"Я тобой горжусь!",
+	"С каждым разом у тебя получается всё лучше!",
+	"Мы с тобой не зря поработали!",
+	"Я вижу, как ты стараешься!",
+	"Ты растешь над собой!",
+	"Ты многое сделал, я это вижу!",
+	"Теперь у тебя точно все получится!"
+	]
 
 
 def fix_marks(child):
@@ -48,15 +46,16 @@ def remove_chastisements(child):
 	Chastisement.objects.filter(schoolkid_id=child.id).delete()
 
 
-def create_commendation(child, school_subject, random_appraisal, lesson):
+def create_commendation(child, school_subject, lesson):
+	random_appraisal = random.choice(APPRAISALS)
 
-	lessons_pool = Lesson.objects.filter(year_of_study=child.year_of_study, 
-                                            group_letter=child.group_letter, 
-                                            subject__title=lesson).order_by('-date')
+	lessons_pool = Lesson.objects.filter(year_of_study=child.year_of_study,
+					group_letter=child.group_letter,
+					subject__title=lesson).order_by('-date')
 
-	Commendation.objects.create(text=random_appraisal, created=lessons_pool.date, 
-                                schoolkid_id=child.id, subject=lessons_pool.subject, 
-                                teacher=lessons_pool.teacher)
+	Commendation.objects.create(text=random_appraisal, created=lessons_pool.date,
+				schoolkid_id=child.id, subject=lessons_pool.subject,
+				teacher=lessons_pool.teacher)
 
 
 def main():
@@ -70,6 +69,7 @@ def main():
 
 	if not name:
 		print("Empty name provided")
+		sys.exit(1)
 	try:
 		child = Schoolkid.objects.get(full_name__contains=name)
 	except Schoolkid.MultipleObjectsReturned:
@@ -81,10 +81,11 @@ def main():
 
 	if not school_subject:
 		print("Empty school_subject provided")
+		sys.exit(1)
 	try:
-        	school_subject = Subject.objects.get(title=school_subject, year_of_study=child.year_of_study)
-    	except Subject.ObjectDoesNotExist:
-        	print(f"{school_subject} not found")
+		school_subject = Subject.objects.get(title=school_subject, year_of_study=child.year_of_study)
+	except Subject.ObjectDoesNotExist:
+		print(f"{school_subject} not found")
 		sys.exit(1)
 		
 	try:
@@ -93,11 +94,10 @@ def main():
 		print("Lesson not found.")
 		sys.exit(1)
 
-
-    fix_marks(child)
-    remove_chastisements(child)
-    create_commendation(child, school_subject, random_appraisal, lesson)
+	fix_marks(child)
+	remove_chastisements(child)
+	create_commendation(child, school_subject, lesson)
 
 
 if __name__ == '__main__':
-    main()
+	main()
